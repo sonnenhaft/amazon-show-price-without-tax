45aa4205fb3c2959fcb6159daee88cece3a16e1b
Vladimir Shein <vlad@cog1.com>
2016-04-07 20:56:02 +0200

    added case with long number with commas and NaN check

--- src/injectable.js
+++ src/injectable.js
@@ -5,22 +5,25 @@
 
     $('.a-color-price').each(function () {
         var self = $(this);
-        var priceBeforeFee = ($.trim(self.text()).slice(1) - 0);
+        var priceBeforeFee = $.trim(self.text()).replace(/[^\d\.\-]/g, '') - 0;
         var shippingValue = $.trim(self.parent().find('.a-color-secondary').text() || '');
         var shippingBeforeFee = 0;
         if ( shippingValue && shippingValue.indexOf('shipping') !== -1 ) {
             shippingBeforeFee = $.trim(shippingValue.replace('+ $', '').replace(' shipping', '')) - 0;
         }
 
-        var title = '$' + removeFeeAndAddPriging(priceBeforeFee + shippingBeforeFee);
-        title += shippingBeforeFee ? ' (include shipping)' : '';
-        self.attr({ title: 'after 15% fee ' + title }).css({ position: 'relative' })
-            .append($('<div>').css({
-                position: 'absolute',
-                color: 'green',
-                top: '-6px',
-                'font-size': '8px',
-                'white-space': 'nowrap'
-            }).text(title));
+        var fullFee = removeFeeAndAddPriging(priceBeforeFee + shippingBeforeFee);
+        if ( !isNaN(fullFee) ) {
+            var title = '$' + fullFee;
+            title += shippingBeforeFee ? ' (include shipping)' : '';
+            self.attr({ title: 'after 15% fee ' + title }).css({ position: 'relative' })
+                .append($('<div>').css({
+                    position: 'absolute',
+                    color: 'green',
+                    top: '-5px',
+                    'font-size': '7px',
+                    'white-space': 'nowrap'
+                }).text(title));
+        }
     });
 })(jQuery, 15);
