ff05be7221b8db868cf3a3fb7424755f720a6c3c
Vladimir Shein <vlad@cog1.com>
2016-04-07 19:47:16 +0200

    updated script to know about shipping

--- src/injectable.js
+++ src/injectable.js
@@ -1,14 +1,26 @@
 (function ( $, taxPercent ) {
-    function formatNumber( number ) {
+    function removeFeeAndAddPriging( number ) {
         return Math.round(number * (100 - taxPercent)) / 100;
     }
 
     $('.a-color-price').each(function () {
         var self = $(this);
-        var oldHover = self.attr('title') || '';
-        var earningAfterTax = (self.text().slice(1) - 0);
-        self.attr({
-            title: oldHover + ' [ dealer will earn: $' + formatNumber(earningAfterTax) + ' after all fees ]'
-        });
+        var priceBeforeFee = ($.trim(self.text()).slice(1) - 0);
+        var shippingValue = $.trim(self.parent().find('.a-color-secondary').text() || '');
+        var shippingBeforeFee = 0;
+        if ( shippingValue && shippingValue.indexOf('shipping') !== -1 ) {
+            shippingBeforeFee = $.trim(shippingValue.replace('+ $', '').replace(' shipping', '')) - 0;
+        }
+
+        var title = '$' + removeFeeAndAddPriging(priceBeforeFee + shippingBeforeFee);
+        title += shippingBeforeFee ? ' (include shipping)' : '';
+        self.attr({ title: 'after 15% fee ' + title }).css({ position: 'relative' })
+            .append($('<div>').css({
+                position: 'absolute',
+                color: 'green',
+                top: '-6px',
+                'font-size': '8px',
+                'white-space': 'nowrap'
+            }).text(title));
     });
 })(jQuery, 15);
